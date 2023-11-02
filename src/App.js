fc53e93ce8c7844bc662d38c00c3f0a190f47aec
Shashank Naik <shashanknaik808@gmail.com>
2023-11-02 12:38:34 +0530

    Have Written Skeleton Structure For Parent Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 
 function App() {
+
+  const celsius = 0;
+  const fahrenheit = 0;
+  const kelvin = 0;
+
+  function onCelciusChange() {
+
+  }
+
+  function onFahreniteChange() {
+
+  }
+
+  function onKelvinChange() {
+
+  }
+
   return (
-    <Demo />
+    <div style={{ "textAlign": "center" }}>
+      <h2>Celsius: <Child temperature={celsius} onTemperatureChange={onCelciusChange} /></h2>
+      <h2>Fahrenheit: <Child temperature={fahrenheit} onTemperatureChange={onFahreniteChange} /></h2>
+      <h2>Kelvin: <Child temperature={kelvin} onTemperatureChange={onKelvinChange} /></h2>
+    </div>
   );
 }
 
---
+++ src/Child.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Child(props) {
+
+    
+}
+
+export default Child;
