fdaf7ecaf7045082d3a550b5dc3e3a260319096d
Shashank Naik <shashanknaik808@gmail.com>
2023-11-02 12:42:32 +0530

    Defined Input Tag in Child Component

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 function Child(props) {
-
-    
+    return (
+        <input style={{ "padding": "10px", "border-radius": "5px" }}
+            value={props.temperature}
+            onChange={handleChange}
+)    
 }
 
 export default Child;
