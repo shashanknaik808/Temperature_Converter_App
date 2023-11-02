5c7d91fffd3cffdf6cdb4676546a1a581b5366cc
Shashank Naik <shashanknaik808@gmail.com>
2023-11-02 12:47:50 +0530

    Wrote a Logic for handleChange Function

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 function Child(props) {
+
+    function handleChange(e) {
+        let num = null;
+        if (e.target.value === '') {
+            num = 0;
+        }
+        else {
+            num.parseInt(e.target.value);
+        }
+
+        props.onTemperatureChange(num);
+    }
     return (
         <input style={{ "padding": "10px", "border-radius": "5px" }}
             value={props.temperature}
             onChange={handleChange}
-)    
+        />
+    )
 }
 
 export default Child;
